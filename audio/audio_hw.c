ad740eb8c457f79600d5f1e98f044a969d0bee90
Dheeraj CVR <cvr.dheeraj@gmail.com>
2015-02-11 10:52:07 +0530

    audio: forward call mute to RIL client interface
    Change-Id: Iccb763b48a687b88692c84976a7cddf72031b5be

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -2670,6 +2670,9 @@ static int adev_set_mic_mute(struct audio_hw_device *dev, bool state)
 {
     struct smdk3xxx_audio_device *adev = (struct smdk3xxx_audio_device *)dev;
 
+    if (adev->mode == AUDIO_MODE_IN_CALL)
+            ril_set_mic_mute(&adev->ril, state);
+
     adev->mic_mute = state;
 
     return 0;

249584f03c1dadbfd977238b1ff2fb1a3ed68a1d
tobigun <hennymcc@yahoo.de>
2015-06-27 02:03:32 +0200

    add degaslte as supported devices
    Change-Id: Id6aeec499522afc572dd14837d3b07a62f38b974

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter gardalte kminilte ,$(TARGET_DEVICE)),)
+ifneq ($(filter gardalte kminilte degaslte ,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 

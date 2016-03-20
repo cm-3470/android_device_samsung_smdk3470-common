8571e4190d790d885fa6d427c58e19038ad0c00e
tobigun <hennymcc@yahoo.de>
2016-03-20 22:41:51 +0100

    patcher: apply patches depending on recovery type (cm vs. twrp)
    Change-Id: I37af72225d87733b3f0ad61bb02cd982a6e61116

--- patch/apply.sh
+++ patch/apply.sh
@@ -4,16 +4,27 @@ MYABSPATH=$(readlink -f "$0")
 PATCHBASE=$(dirname "$MYABSPATH")
 CMBASE=$(readlink -f "$PATCHBASE/../../../../")
 
-for i in $(find "$PATCHBASE"/* -type d); do
-	PATCHNAME=$(basename "$i")
-	PATCHTARGET=$PATCHNAME
-	for j in $(seq 4); do
-		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
-		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
-	done
+cd "$CMBASE/bootable/recovery"
+if git remote -v | grep omnirom; then
+    TWRP=true
+else
+    TWRP=false
+fi
 
-	echo
-	echo applying $PATCHNAME to $PATCHTARGET
-	cd "$CMBASE/$PATCHTARGET" || exit 1
-	git am --ignore-whitespace -3 "$PATCHBASE/$PATCHNAME"/*.patch || exit 1
+for i in $(find "$PATCHBASE"/* -type d); do
+    PATCHNAME=$(basename "$i")
+    PATCHTARGET=$PATCHNAME
+    for j in $(seq 4); do
+        PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
+        if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+    done
+
+    echo
+    echo applying $PATCHNAME to $PATCHTARGET
+    cd "$CMBASE/$PATCHTARGET" || exit 1
+
+    suffix=".patch"
+    if [ $PATCHNAME = "bootable_recovery" -a $TWRP = true ]; then suffix=".twrp.patch"; fi
+    if [ $PATCHNAME = "bootable_recovery" -a $TWRP = false ]; then suffix=".cm.patch"; fi
+    git am --ignore-whitespace -3 "$PATCHBASE/$PATCHNAME"/*${suffix} || exit 1
 done

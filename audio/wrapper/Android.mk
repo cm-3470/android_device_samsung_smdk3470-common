f999a3e4ddc2ed5be3d0b74848d696c3849cffbb
tobigun <hennymcc@yahoo.de>
2016-03-28 16:58:59 +0200

    audio: remove unused ril_interface from wrapper
    ril_interface is incompatible with stock audio lib. Slightl different
    code is included in libaudio-ril.so.
    
    Change-Id: I7e8cdfa7f28946bcf3d2a1b1b7cba829a4a34039

--- audio/wrapper/Android.mk
+++ audio/wrapper/Android.mk
@@ -25,15 +25,12 @@ LOCAL_MODULE := audio.primary.$(TARGET_BOOTLOADER_BOARD_NAME)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_SRC_FILES := audio_hw.c ril_interface.c
+LOCAL_SRC_FILES := audio_hw.c
 
 LOCAL_C_INCLUDES += \
-	external/tinyalsa/include \
-	$(call include-path-for, audio-effects) \
-	$(call include-path-for, audio-utils) \
-	$(call include-path-for, audio-route)
+	external/tinyalsa/include
 
-LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libaudioroute libdl libhardware
+LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libdl libhardware
 
 include $(BUILD_SHARED_LIBRARY)
 

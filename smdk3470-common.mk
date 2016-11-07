bb59c8126a0fa9ae1b1f8e56e80470e524820ccb
Tobias Gunkel <hennymcc@yahoo.de>
2016-11-07 21:57:08 +0100

    media_codecs: move to device specific repo
    Change-Id: Idf2abae25695c643386dff66822c2116e92677a7

--- smdk3470-common.mk
+++ smdk3470-common.mk
@@ -46,7 +46,6 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/media/media_codecs.xml:system/etc/media_codecs.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml

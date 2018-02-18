6fb951fcd55bed892aef2d9c041763d1103e2ed0
Tobias Gunkel <hennymcc@yahoo.de>
2018-02-18 10:36:24 +0100

    audio-source: remove old experimental versions of open-source audio hal
    Change-Id: I4f46a56899585eeb34771af006c4500353a1091a

--- audio/source/experimental/working_question/old/install.sh
+++
@@ -1,4 +0,0 @@
-mkdir -p /media/sf_shared/selected-cm13/etc
-mkdir -p /media/sf_shared/selected-cm13/lib/hw
-cp /mnt/cm13/cm13/out/target/product/kminilte/system/lib/hw/audio.primary.universal3470.so /media/sf_shared/selected-cm13/lib/hw
-cp /mnt/cm13/cm13/out/target/product/kminilte/system/etc/mixer_paths.xml /media/sf_shared/selected-cm13/etc
